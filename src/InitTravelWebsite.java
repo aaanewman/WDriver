e047b397226b0fb1b143d457618876137a5fee9e
Anthony Newman <aaanewman@gmail.com>
2012-05-29 11:27:38 +1000

    Init automation complete

---
+++ src/InitTravelWebsite.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.*;
+import static org.junit.Assert.*;
+
+public class InitTravelWebsite {
+
+	private WebDriver driver;
+	
+	public InitTravelWebsite(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void automateInitTravelWebsite() {
+		driver.get("http://newtours.demoaut.com/");
+
+	}
+	
+	public void test_automateInitTravelWebsite() {
+		//assert that the current page URL is the same as the expected URL.
+		assertEquals("http://newtours.demoaut.com/", driver.getCurrentUrl());
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,28 @@
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
 import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class Main {
 	private WebDriver driver;
-	private String baseUrl;
-	private StringBuffer verificationErrors = new StringBuffer();
 	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		driver = new FirefoxDriver();
-		baseUrl = "http://newtours.demoaut.com/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void login_Test() throws Exception {
-		driver.get(baseUrl + "/");
-		
-		//assert that the current page URL is the same as the expected URL.
-		assertEquals("http://newtours.demoaut.com/", driver.getCurrentUrl());
-		
-		driver.findElement(By.name("userName")).clear();
-		driver.findElement(By.name("userName")).sendKeys("mercury");
-		driver.findElement(By.name("password")).clear();
-		driver.findElement(By.name("password")).sendKeys("mercury");
-		driver.findElement(By.name("login")).click();
-		
-		System.out.print(driver.getCurrentUrl());
-		
-		//assert that the current page URL is the same as the expected URL.
-		assertEquals("http://newtours.demoaut.com/", driver.getCurrentUrl());
-
+	public void init_Test(){
+		InitTravelWebsite initTravelWebsite = new InitTravelWebsite(driver);
+		initTravelWebsite.automateInitTravelWebsite();
+		initTravelWebsite.test_automateInitTravelWebsite();
 	}
+	
+	
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown(){
 		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
 	}
 
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
 }
